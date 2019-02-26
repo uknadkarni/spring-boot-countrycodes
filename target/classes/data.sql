71caadb28c69fec0269d0515985415400ec6568e
Utkarsh Nadkarni <utkarsh.nadkarni@gmail.com>
2019-02-26 09:04:14 -0500

    Added support for H2

---
+++ target/classes/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO phonebook (name, number ) VALUES ('India', '91');
+INSERT INTO phonebook (name, number ) VALUES ('Brazil', '55');
+INSERT INTO phonebook (name, number ) VALUES ('USA', '01');
+INSERT INTO phonebook (name, number ) VALUES ('Italy', '39');
